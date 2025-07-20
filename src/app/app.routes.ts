49921629c99dd835ec0695e066dff3e96b844e42
saerx <saerlaith.robyn@gmail.com>
2025-07-20 22:40:08 +0100

    adds other resources component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { AudiencesComponent } from './audiences/audiences.component';
 import { HomeComponent } from './home/home.component';
+import { OtherResourcesComponent } from './other-resources/other-resources.component';
 import { PlanningComponent } from './planning/planning.component';
 import { ProductionComponent } from './production/production.component';
 import { WorkplaceComponent } from './workplace/workplace.component';
@@ -36,5 +37,10 @@ export const routes: Routes = [
     path: 'audiences',
     component: AudiencesComponent,
     title: 'Audiences',
+  },
+  {
+    path: 'other-resources',
+    component: OtherResourcesComponent,
+    title: 'Other Resources',
   },
 ];
---
+++ src/app/other-resources/other-resources.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-other-resources',
+  imports: [],
+  templateUrl: './other-resources.component.html',
+  styleUrl: './other-resources.component.scss'
+})
+export class OtherResourcesComponent {
+
+}

be506df2a5727df0c1fb1d193841b1855cb663a7
saerx <saerlaith.robyn@gmail.com>
2025-10-04 14:40:13 +0100

    Added first draft of accessibility page

--- src/app/accessibility/accessibility.component.ts
+++ src/app/accessibility/accessibility.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-accessibility',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './accessibility.component.html',
-  styleUrl: './accessibility.component.scss'
+  styleUrl: './accessibility.component.scss',
 })
-export class AccessibilityComponent {
-
-}
+export class AccessibilityComponent {}
